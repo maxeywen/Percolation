52c232d1074c89ef49a8e4a50a7d5a9c260eb33c
Max Wen <nobody@nowhere.com>
2017-02-15 17:40:07 +0700

    running PercolationStats from main() and with random generator as stub for actual trials

--- src/java/PercolationStats.java
+++ src/java/PercolationStats.java
@@ -5,27 +5,57 @@ public class PercolationStats {
 
   int n;
   int numTrials;
+  double[] results;
+  private double mean;
 
   // perform trials independent experiments on an n-by-n grid
   public PercolationStats(int n, int trials) {
+    // The constructor should throw a java.lang.IllegalArgumentException if
+    // either n ≤ 0 or trials ≤ 0
+    if (n <= 0 || trials < 0) {
+      throw new IllegalArgumentException();
+    }
+
     this.n = n;
     this.numTrials = trials;
+    results = new double[numTrials];
+
+    for (int trial = 0; trial < numTrials; trial++) {
+      double x = StdRandom.uniform(0.50, 0.70);
+      results[trial] = x;
+      System.out.println(x);
+    }
+
+    mean = mean();
+    System.out.println("mean                    = " + mean);
+    double stddev = stddev();
+    System.out.println("stddev                  = " + stddev);
+
+    double x1 = confidenceLo();
+    double x2 = confidenceHi();
+    System.out.println("95% confidence interval = [" + x1 + ", " + x2 + "]");
   }
 
   // sample mean of percolation threshold
-  public double mean() { return -1.0; }
+  public double mean() { return StdStats.mean(results); }
 
   // sample standard deviation of percolation threshold
-  public double stddev() { return -1.0; }
+  public double stddev() { return StdStats.stddev(results); }
 
   // low  endpoint of 95% confidence interval
-  public double confidenceLo() { return -1.0; }
+  public double confidenceLo() {
+    double lo = mean - (1.96 * StdStats.var(results))/Math.sqrt(numTrials);
+    return lo;
+  }
 
   // high endpoint of 95% confidence interval
-  public double confidenceHi() { return -1.0; }
+  public double confidenceHi() {
+    double hi = mean + (1.96 * StdStats.var(results))/Math.sqrt(numTrials);
+    return hi;
+  }
 
   // test client (described below)
   public static void main(String[] args) {
-    PercolationStats ps = new PercolationStats(1,1);
+    PercolationStats ps = new PercolationStats(200,100);
   }
 }
